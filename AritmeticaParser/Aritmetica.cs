7bdea1214f31916afdb35df58070be5a0ccf857e
TineoC <christophertineo02@gmail.com>
2022-11-17 12:58:29 -0400

    Add project files.

---
+++ AritmeticaParser/Aritmetica.cs
@@ -0,0 +1,38 @@
+﻿using Antlr4.Runtime.Misc;
+using Antlr4.Runtime.Tree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static aritmeticaParser;
+
+namespace AritmeticaParser
+{
+    internal class Aritmetica : aritmeticaBaseVisitor<int>
+    { 
+        public override int VisitAritmetica([NotNull] AritmeticaContext context)
+        {
+            Console.WriteLine($"Visitando Aritmética: {context.GetText()}");
+            return base.VisitAritmetica(context);
+        }
+
+        public override int VisitExpresion([NotNull] ExpresionContext context)
+        {
+            Console.WriteLine($"Visitando Aritmética: {context.GetText()}");
+            return base.VisitExpresion(context);
+        }
+
+        public override int VisitFactor([NotNull] FactorContext context)
+        {
+            Console.WriteLine($"Visitando Aritmética: {context.GetText()}");
+            return base.VisitFactor(context);
+        }
+
+        public override int VisitTermino([NotNull] TerminoContext context)
+        {
+            Console.WriteLine($"Visitando Aritmética: {context.GetText()}");
+            return base.VisitTermino(context);
+        }
+    }
+}
---
+++ AritmeticaParser/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Antlr4.Runtime;
+using AritmeticaParser;
+using static aritmeticaParser;
+
+string input = "5*6+9*3-1";
+
+Console.WriteLine($"Input : {input}\n");
+
+ICharStream stream = CharStreams.fromString(input);
+
+aritmeticaLexer lexer = new aritmeticaLexer(stream);
+
+CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+aritmeticaParser parser = new aritmeticaParser(tokens);
+
+AritmeticaContext tree = parser.aritmetica();
+
+Aritmetica aritmetica = new Aritmetica();
+
+int res = aritmetica.Visit(tree);
